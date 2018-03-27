a4722cd11a41bdbc965483b1556ea099d23dad4f
Lucas Augusto <lucas.augusto5061@gmail.com>
2018-03-26 21:23:24 -0300

    adicionado componente de sobre nós

---
+++ src/about/index.jsx
@@ -0,0 +1,19 @@
+import Header from '../header'
+import Form from '../users/form'
+
+import React, { Component } from 'react'
+
+class Home extends Component {
+  render () {
+    return (
+      <div className='row'>
+        <Header title='Sobre nós' />
+        <div className='cadastro col-lg-12'>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim tempore rerum adipisci illo sequi ex expedita cumque vel veniam odio aut, laudantium ipsum, architecto eos possimus atque? Reiciendis quis magni minus nemo dolore, neque vel? Obcaecati ullam provident quam accusamus, voluptatem optio voluptas consequuntur velit nemo, rem delectus. Veritatis accusantium alias beatae quo fugit ducimus, suscipit laudantium itaque id earum minima reprehenderit quis saepe eligendi explicabo nesciunt necessitatibus inventore quam autem? Explicabo nesciunt veniam quas impedit laboriosam incidunt? Assumenda rem saepe quisquam nisi incidunt culpa sint quam illum? Eum, esse veritatis molestiae et quam asperiores suscipit beatae cumque dicta culpa?
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Home
